baf7ad9918f51433f5483b7166a9723b440cbf43
Paul Tudan <ptudan@udel.edu>
2018-05-09 16:26:13 -0400

    restructured scheducler to job and controller

--- Job.h
+++ Job.h
@@ -1,9 +1,17 @@
 //Paul Tudan and Jefferson Kappers
 //5-9-18
 
+#include <string>
+using namespace std;
+
 class Job{
 public:
-	string currentStatus();
+	void initiate(int arrivalTime, int ID, int memoryNeed, int maxDevices, int priority, int duration);
+	String currentStatus();
+	void quantamStep(int quantam);
+	void releaseDevice(int num);
+	void addDevice(int num);
+	int requestDevice(int num);
 
 private:
 	int arrivalTime;
