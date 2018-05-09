6a2b5bea1ab743c63c6c3ed36139c48aa93859e5
Paul Tudan <ptudan@udel.edu>
2018-05-09 15:55:57 -0400

    added function headers to job class

--- scheduler.h
+++ scheduler.h
@@ -1,9 +1,17 @@
 //Paul Tudan and Jefferson Kappers
 //5-9-18
+#include <string>
+using namespace std;
 
 class Job{
 public:
-	string currentStatus();
+	void initiate(int arrivalTime, int completionTime, int ID, int memoryNeed, int maxDevices, int priority, int duration);
+	String currentStatus();
+	void quantamStep(int quantam);
+	void releaseDevice(int num);
+	void addDevice(int num);
+	int requestDevice(int num);
+
 
 private:
 	int arrivalTime;
@@ -12,6 +20,7 @@ private:
 	int memoryNeed;
 	int maxDevices;
 	int currentDevices;
+	int devicesRequested;
 	int priority;
 	int duration;
 	int progress;
