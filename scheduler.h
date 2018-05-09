92c0a350ebf904a35368089c14fcdecfb76f4e91
Paul Tudan <ptudan@udel.edu>
2018-05-09 15:15:05 -0400

    created header function, started working on job class

---
+++ scheduler.h
@@ -0,0 +1,19 @@
+//Paul Tudan and Jefferson Kappers
+//5-9-18
+
+class Job{
+public:
+	string currentStatus();
+
+private:
+	int arrivalTime;
+	int completionTime;
+	int ID;
+	int memoryNeed;
+	int maxDevices;
+	int currentDevices;
+	int priority;
+	int duration;
+	int progress;
+
+}
