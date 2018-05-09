9ab8a5adc5a69e9aa4bcf2a3247e45e47d382f1b
Jefferson D Kappers <thisisnotjeffsemail@gmail.com>
2018-05-09 15:49:33 -0400

    Update main.cpp
    added some stuff to read input from text file formatting coming up

--- main.cpp
+++ main.cpp
@@ -3,8 +3,24 @@
 //5-9-18
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
+int main(int argc, char **argv) {
+    string testOutputSting
+    ofstream inputFile;
+    inputFile.open('input.txt');
+    if (inputFile.is_open()) {
+        while (!inputFile.eof) {
+            getline(inputFile,oneLineString);
+            cout<<oneLineString;
+            for (i = 0; i <= oneLineString.size(); i++) {
 
-int main(int argc, char **argv){
-
+            }
+        }
+    }
+    else {
+        cout << "Unable to open file";
+    }
+    return 0
 }
